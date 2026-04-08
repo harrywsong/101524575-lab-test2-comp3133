b1fd8b7bb18eb6ff7972f8848cbfe12cddf372aa
Woohyuk Song <harrywsong@naver.com>
2026-04-08 12:52:58 -0400

    add characterfilter

--- src/app/components/characterfilter/characterfilter.ts
+++ src/app/components/characterfilter/characterfilter.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { CharacterService } from '../../services/character';
+import { Character } from '../../models/character';
 
 @Component({
   selector: 'app-characterfilter',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterLink],
   templateUrl: './characterfilter.html',
-  styleUrl: './characterfilter.css',
+  styleUrl: './characterfilter.css'
 })
-export class Characterfilter {}
+export class Characterfilter {
+  houses = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'];
+  selectedHouse = signal('Gryffindor');
+  characters = signal<Character[]>([]);
+
+  constructor(private characterService: CharacterService) {
+    this.loadHouse('Gryffindor');
+  }
+
+  onHouseChange(house: string): void {
+    this.selectedHouse.set(house);
+    this.loadHouse(house);
+  }
+
+  loadHouse(house: string): void {
+    this.characterService.getCharactersByHouse(house).subscribe(data => {
+      this.characters.set(data);
+    });
+  }
+}
