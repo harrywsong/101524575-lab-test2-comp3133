144427397a7185ba097d9462f22e9be4fe4d4cd2
Woohyuk Song <harrywsong@naver.com>
2026-04-08 12:35:13 -0400

    add character service

--- src/app/services/character.ts
+++ src/app/services/character.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Character } from '../models/character';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class Character {}
+@Injectable({ providedIn: 'root' })
+export class CharacterService {
+  constructor(private http: HttpClient) {}
+
+  getAllCharacters(): Observable<Character[]> {
+    return this.http.get<Character[]>(`https://hp-api.onrender.com/api/characters`);
+  }
+
+  getCharactersByHouse(house: string): Observable<Character[]> {
+    return this.http.get<Character[]>(`https://hp-api.onrender.com/api/characters/house/${house}`);
+  }
+
+  getCharacterById(id: string): Observable<Character[]> {
+    return this.http.get<Character[]>(`https://hp-api.onrender.com/api/characters/${id}`);
+  }
+}
