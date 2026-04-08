ea628000f1c6edd3756357f880e376f010e94f71
Woohyuk Song <harrywsong@naver.com>
2026-04-08 11:38:56 -0400

    add characterlist.ts

--- src/app/components/characterlist/characterlist.ts
+++ src/app/components/characterlist/characterlist.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Character } from '../../models/character';
+import { HarryPotter } from '../../services/harry-potter';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-characterlist',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './characterlist.html',
   styleUrl: './characterlist.css',
 })
-export class Characterlist {}
+export class Characterlist {
+  characters: Character[] = [];
+
+  // fetch the characters from the service when the component is initialized
+  constructor(private hpService: HarryPotter, private router: Router) {
+    this.hpService.getCharacters().subscribe((data) => {
+      this.characters = data;
+    });
+  }
+
+  // clicking on a character will navigate to the character detail page
+  goTo(id: string) {
+    this.router.navigate(['/characters', id]);
+  }
+}
