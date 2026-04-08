9f600688d796f735adc2d749f952af5c665f3632
Woohyuk Song <harrywsong@naver.com>
2026-04-08 12:38:23 -0400

    add routes and created components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Characterlist } from './components/characterlist/characterlist';
+import { Characterfilter } from './components/characterfilter/characterfilter';
+import { Characterdetails } from './components/characterdetails/characterdetails';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Characterlist },
+  { path: 'filter', component: Characterfilter },
+  { path: 'character/:id', component: Characterdetails }
+];
