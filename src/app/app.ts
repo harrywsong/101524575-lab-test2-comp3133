ff95f775ed57bd16036775c4647dd98445087bc8
Woohyuk Song <harrywsong@naver.com>
2026-04-08 12:39:26 -0400

    add app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('101524575-lab-test2-comp3133');
+  protected readonly title = signal('Harry Potter Characters');
 }
