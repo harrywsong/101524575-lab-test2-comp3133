ab4400d31b857a023652e8597cdd20da4bd64e52
Woohyuk Song <harrywsong@naver.com>
2026-04-08 12:40:19 -0400

    add characterlist.ts

--- src/app/components/characterlist/characterlist.ts
+++ src/app/components/characterlist/characterlist.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CharacterService } from '../../services/character';
+import { Character } from '../../models/character';
 
 @Component({
   selector: 'app-characterlist',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './characterlist.html',
-  styleUrl: './characterlist.css',
+  styleUrl: './characterlist.css'
 })
-export class Characterlist {}
+export class CharacterlistComponent implements OnInit {
+  characters = signal<Character[]>([]);
+
+  constructor(private characterService: CharacterService) {}
+
+  ngOnInit(): void {
+    this.characterService.getAllCharacters().subscribe(data => {
+      this.characters.set(data);
+    });
+  }
+}
